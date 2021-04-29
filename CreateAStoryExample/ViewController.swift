1bc96256fa08ae9e4a4010ca34e979e040e78063
IvanNextToJunior <Vanwan@bk.ru>
2021-04-29 21:55:02 +0300

    Update ViewController.swift

--- CreateAStoryExample/ViewController.swift
+++ CreateAStoryExample/ViewController.swift
@@ -22,23 +22,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondChoiceButton: UIButton!
     
     @IBAction func chooseAStoryAction(_ sender: UIButton) {
-       
+        
         setUpAStory()
         sender.isSelected = !sender.isSelected
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        storyLabel.text = ""
+    }
+    
     private func setUpAStory() {
         
-       
-    
-}
-
-    private func setup () {
-       
-        if firstChoiceButton.isSelected || secondChoiceButton.isSelected {
-            firstChoiceButton.setTitle(<#T##title: String?##String?#>, for: <#T##UIControl.State#>)
+        setup()
+        changeIndex()
+        
+        if firstChoiceButton.isSelected {
+            storyLabel.text = stories[firstStoryNumber]
         }
+        
+        else {
+            storyLabel.text = stories[secondStoryNumber]
+        }
+        
     }
-
+    
+    private func setup () {
+        
+        firstChoiceButton.setTitle(choicesInTheStory[firstStoryNumber], for: .normal)
+        
+        secondChoiceButton.setTitle(choicesInTheStory[secondStoryNumber], for: .normal)
+        
+    }
+    
+    private func changeIndex () {
+       
+        if storyLabel.text != "" && firstStoryNumber < stories.count && secondStoryNumber < stories.count && firstStoryNumber != secondStoryNumber {
+        
+            firstStoryNumber = secondStoryNumber + 1
+        secondStoryNumber = firstStoryNumber + 1
+       
+        }
+        else if firstStoryNumber == stories.count - 2 && secondStoryNumber == stories.count - 1 && (storyLabel.text == stories[firstStoryNumber] || storyLabel.text == stories[secondStoryNumber]) && (firstChoiceButton.isSelected || secondChoiceButton.isSelected) {
+          
+            firstStoryNumber = 0
+            secondStoryNumber = 1
+        }
+    
+    }
+    
 }
