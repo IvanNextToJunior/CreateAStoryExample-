1061882d3dac00afa9559f593377b771a71a8d6c
IvanNextToJunior <Vanwan@bk.ru>
2021-06-02 15:27:28 +0300

    Update ViewController.swift

--- CreateAStoryExample/ViewController.swift
+++ CreateAStoryExample/ViewController.swift
@@ -21,33 +21,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondChoiceButton: UIButton!
     
     @IBAction func chooseAStoryAction(_ sender: UIButton) {
+        
         setUpAStory()
         sender.isSelected.toggle()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         firstChoiceButton.setTitle(choicesInTheStory[0], for: .normal)
         secondChoiceButton.setTitle(choicesInTheStory[1], for: .normal)
         storyLabel.text = "Story"
     }
     
     private func setUpAStory() {
-     
+        
         storyLabel.text = firstChoiceButton.isEnabled ? stories[firstStoryNumber] : stories[secondStoryNumber]
-      
-        if (firstStoryNumber == choicesInTheStory.count - 2 || secondStoryNumber == choicesInTheStory.count - 1) && (storyLabel.text == stories[firstStoryNumber] || storyLabel.text == stories[secondStoryNumber]) {
-          
+       
+        if firstStoryNumber < choicesInTheStory.count - 2, secondStoryNumber < choicesInTheStory.endIndex {
+            firstStoryNumber = secondStoryNumber + 1
+            secondStoryNumber = firstStoryNumber + 1
+        }
+        else if firstStoryNumber == choicesInTheStory.endIndex-1, secondStoryNumber == choicesInTheStory.endIndex, firstChoiceButton.isEnabled || secondChoiceButton.isEnabled {
+            
+            storyLabel.text = firstChoiceButton.isEnabled ? stories[firstStoryNumber] : stories[secondStoryNumber]
             firstStoryNumber = 0
             secondStoryNumber = 1
         }
-       
-        firstStoryNumber = secondStoryNumber + 1
-        secondStoryNumber = firstStoryNumber + 1
-       
+        else {
+            firstStoryNumber = 0
+            secondStoryNumber = 1
+            storyLabel.text = "The end"
+        }
         firstChoiceButton.setTitle(choicesInTheStory[firstStoryNumber], for: .normal)
         secondChoiceButton.setTitle(choicesInTheStory[secondStoryNumber], for: .normal)
+        
+      
+        
+        
+        
     }
-    
 }
