cda05ba10baa4cb74fa136ad924c479c4f09feff
IvanNextToJunior <58988299+IvanNextToJunior@users.noreply.github.com>
2025-02-06 19:24:01 +0300

    refactor

--- CreateAStoryExample/ViewController.swift
+++ CreateAStoryExample/ViewController.swift
@@ -10,86 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    ///Индексы для словаря
-    private var firstStoryNumber = 0
-    private var secondStoryNumber = 1
+    let storyNamesArray = ["Jungle book", "Once upon a time", "Perks of Being a Walflower", "Blue Lagoon", "Spider Man", "Iron Man"]
+
+    let storyFormatChoices = ["Listen to Jungle Book story", "Listen to Once upon a time story", "Listen to Perks of being a Walflower story", "Read Blue Lagoon", "Read Spider Man","Watch Iron Man"]
     
+    @IBOutlet weak private var storyLabel: UILabel!
     
-    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak private var firstChoiceButton: UIButton!
     
-    @IBOutlet weak var firstChoiceButton: UIButton!
+    @IBOutlet weak private var secondChoiceButton: UIButton!
     
-    @IBOutlet weak var secondChoiceButton: UIButton!
-    
-    @IBAction func chooseAStoryAction(_ sender: UIButton) {
-    setUpAStory()
+    @IBAction private func chooseAStoryAction(_ sender: UIButton) {
+      
+        if sender.isSelected {
+            setUp()
+        }
+       
         sender.isSelected = !sender.isSelected
     }
     
-    
-    
-    
+    private func setUp () {
+        
+        for i in 0...storyNamesArray.count-1 {
+            storyLabel.text = storyNamesArray[i]
+        }
+        
+        for j in 0...storyFormatChoices.count-2 {
+            firstChoiceButton.setTitle(storyFormatChoices[j], for: .normal)
+            secondChoiceButton.setTitle(storyFormatChoices[j+1], for: .normal)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
   
     }
     
-//   private func createADictionary ()  -> [String: String] {
-//
-//    var dict: [String: String] = [:]
-//
-//        for storyChoice in 0...storyFormatChoices.count-1 {
-//
-//            for storyName in 0...storyNamesArray.count-1 {
-//
-//                let endOfAStory = storyFormatChoices[storyChoice]
-//                let story = storyNamesArray[storyName]
-//
-//                if  endOfAStory == story {
-//                    dict.updateValue(endOfAStory, forKey: story)
-//                }
-//            }
-//        }
-//   return dict
-//
-//   }
-    private func setUpAStory() {
-        
-     
-        if firstChoiceButton.isSelected && firstStoryNumber <= 3 || firstStoryNumber == 0{
-    
-            storyLabel.text = storyNamesArray[firstStoryNumber]
-            
-            firstChoiceButton.setTitle(storyFormatChoices[firstStoryNumber], for: .selected)
-            secondChoiceButton.setTitle(storyFormatChoices[secondStoryNumber], for: .selected)
-            
-            
-        }
-        
-        else if secondChoiceButton.isSelected && secondStoryNumber <= 4 || secondStoryNumber == 1 {
-           secondStoryNumber += 1
-            storyLabel.text = storyNamesArray[secondStoryNumber]
-            firstChoiceButton.setTitle(storyFormatChoices[firstStoryNumber], for: .selected)
-            secondChoiceButton.setTitle(storyFormatChoices[secondStoryNumber], for: .selected)
-        }
-       
-        else if  firstStoryNumber >= 5 || secondStoryNumber >= 6{
-          
-            firstChoiceButton.setTitle("Choice A", for: .normal)
-            secondChoiceButton.setTitle("Choice B", for: .normal)
-            storyLabel.text = ""
-      
-        }
-        
-        firstStoryNumber = secondStoryNumber + 1
-        secondStoryNumber = firstStoryNumber + 1
-       
-       
-    }
+  
+
+   }
+   
    
     
-    }
+    
 
 
 
